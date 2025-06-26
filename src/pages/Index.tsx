ded972da5de5ba0fcd306c9d4f312caecb7f17d6
Anuj Kumar <2023eeb1187@iitrpr.ac.in>
2025-06-26 21:49:07 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -4,7 +4,7 @@ import Header from '../components/Header';
 import Hero from '../components/Hero';
 import About from '../components/About';
 import Skills from '../components/Skills';
-import Experience from '../components/Experience';
+// import Experience from '../components/Experience';
 import Projects from '../components/Projects';
 import Contact from '../components/Contact';
 import Footer from '../components/Footer';
@@ -17,7 +17,7 @@ const Index = () => {
       <Hero />
       <About />
       <Skills />
-      <Experience />
+{/*       <Experience /> */}
       <Projects />
       <Contact />
       <Footer />
